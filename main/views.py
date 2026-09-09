92ca1a7cc8050f2c28aa5f287ffd9fc392fed764
Vhino <ervhinoaryoseto@gmail.com>
2026-09-09 17:37:08 +0700

    feat: Penambahan section experience dan Konsep MVT

--- main/views.py
+++ main/views.py
@@ -18,7 +18,7 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Burhan",
+        "name": "Ervhino Aryo Seto",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
